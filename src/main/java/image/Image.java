42dfb84c502517ac3304107db009008e90a38cf8
nithisht <nithisht@thoughtworks.com>
2019-12-09 11:37:58 +0530

    day8 1

--- src/main/java/image/Image.java
+++ src/main/java/image/Image.java
@@ -4,10 +4,14 @@ import com.google.common.collect.Iterators;
 import com.google.common.collect.Streams;
 import com.google.common.collect.UnmodifiableIterator;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
 public class Image {
+    public static final int TRANSPARENT_COLOR = 2;
     private List<Layer> layer;
 
     public Image(List<Layer> layer) {
@@ -18,11 +22,20 @@ public class Image {
         return layer;
     }
 
-    public static Image createImage(int widht, int height, List<Integer> sifCode) {
-        final UnmodifiableIterator<List<Integer>> sifCodedLayers = Iterators.partition(sifCode.iterator(), widht * height);
+    public static Image createImage(int width, int height, String image){
+        final List<Integer> sifCodes = image.chars()
+          .map(c -> Integer.parseInt("" + (char) c))
+          .boxed()
+          .collect(Collectors.toList());
+
+        return createImage(width,height,sifCodes);
+    }
+
+    public static Image createImage(int width, int height, List<Integer> sifCode) {
+        final UnmodifiableIterator<List<Integer>> sifCodedLayers = Iterators.partition(sifCode.iterator(), width * height);
 
         final List<Layer> layers = Streams.stream(sifCodedLayers)
-          .map(aSifCodedLayer -> new Layer(widht, height, aSifCodedLayer))
+          .map(aSifCodedLayer -> new Layer(width, height, aSifCodedLayer))
           .collect(Collectors.toList());
 
         return new Image(layers);
@@ -33,7 +46,7 @@ public class Image {
         long minNumOfZerosPerLayerTillNow = Integer.MAX_VALUE;
         Layer leastCorrupted = null;
         for (Layer aLayer:layer) {
-            final long countOfZeros = aLayer.getaSifCodedLayer().stream()
+            final long countOfZeros = aLayer.getSifCode().stream()
               .mapToInt(Integer::intValue)
               .filter(value -> value == 0)
               .count();
@@ -44,5 +57,38 @@ public class Image {
             }
         }
         return leastCorrupted;
+    }
+
+
+    public Layer processImage(){
+        final int width = getLayer().get(0).getWidth();
+        final int height = getLayer().get(0).getHeight();
+
+        int res[]=new int[width*height];
+        for (int i = 0; i < res.length; i++) {
+            res[i]=getFirstNonTransparentPixel(i);
+        }
+        return new Layer(width,height, Arrays.stream(res).boxed().collect(Collectors.toList()));
+    }
+
+    private int getFirstNonTransparentPixel(int i) {
+        final OptionalInt first = getLayer().stream()
+          .mapToInt(layer -> layer.getValueAt(i))
+          .filter(color -> color != TRANSPARENT_COLOR)
+          .findFirst();
+        return first.orElse(TRANSPARENT_COLOR);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(layer, image.layer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(layer);
     }
 }
--- src/main/java/image/Layer.java
+++ src/main/java/image/Layer.java
@@ -1,17 +1,19 @@
 package image;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Layer {
     private final int width;
     private final int height;
-    private final List<Integer> aSifCodedLayer;
+    private final List<Integer> sifCode;
 
 
-    public Layer(int width, int height, List<Integer> aSifCodedLayer) {
+    public Layer(int width, int height, List<Integer> sifCode) {
         this.width = width;
         this.height = height;
-        this.aSifCodedLayer = aSifCodedLayer;
+        this.sifCode = new ArrayList<>(sifCode);
     }
 
     public int getWidth() {
@@ -22,7 +24,50 @@ public class Layer {
         return height;
     }
 
-    public List<Integer> getaSifCodedLayer() {
-        return aSifCodedLayer;
+    public List<Integer> getSifCode() {
+        return sifCode;
+    }
+
+    public int getValueAt(int index){
+        return sifCode.get(index);
+    }
+
+    public String print() {
+        StringBuilder builder = new StringBuilder();
+//        for (int i = 0; i < height; i++) {
+//            for (int j = 0; j <width; j++) {
+//                builder.append(sifCode.get((height*i)+j));
+//            }
+//            builder.append('\n');
+//        }
+//        return builder.deleteCharAt(builder.length()-1).toString();
+
+        for (int i = 0; i < sifCode.size(); i++) {
+            if(i!=0&&i%width==0){
+                builder.append('\n');
+            }
+            builder.append(sifCode.get(i));
+        }
+        return builder.toString();
+    }
+
+    public String specialPrint() {
+        return print().replace("0"," ");
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Layer layer = (Layer) o;
+        return width == layer.width &&
+          height == layer.height &&
+          Objects.equals(sifCode, layer.sifCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height, sifCode);
     }
 }

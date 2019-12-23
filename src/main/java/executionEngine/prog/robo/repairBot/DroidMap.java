64ecee6c667ec630c4ceb95878d2c5db1619a3ed
nithisht <nithisht@thoughtworks.com>
2019-12-23 19:54:03 +0530

    day 15 2 refactor

--- src/main/java/executionEngine/prog/robo/repairBot/DroidMap.java
+++ src/main/java/executionEngine/prog/robo/repairBot/DroidMap.java
@@ -3,22 +3,20 @@ package executionEngine.prog.robo.repairBot;
 import executionEngine.prog.robo.Direction;
 import executionEngine.prog.robo.Location;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class DroidMap {
-    private final Set<DroidState> unexploredStates = new TreeSet<>();
-    private final Set<Location> visitedLocation = new HashSet<>();
     private final long[] program;
+    private final Set<DroidState> unexploredStates = new TreeSet<>();
+    private final Map<Location, Character> visitedLocation = new HashMap<>();
 
     public DroidMap(long... program) {
         this.program = program;
     }
 
     private void addToStateAllPossibleMoves(RepairDroid droid, int step) {
-        if(visitedLocation.add(droid.getCurrentLocation())) {
+        if(!visitedLocation.keySet().contains(droid.getCurrentLocation())) {
+            visitedLocation.put(droid.getCurrentLocation(), '_');
             addState(droid, Direction.UP, step + 1);
             addState(droid, Direction.DOWN, step + 1);
             addState(droid, Direction.LEFT, step + 1);
@@ -44,6 +42,25 @@ public class DroidMap {
         }
         return Integer.MAX_VALUE;
     }
+
+    public Map<Location, Character> mapAll() {
+        addToStateAllPossibleMoves(new RepairDroid(program), 0);
+        while (!unexploredStates.isEmpty()) {
+            final Iterator<DroidState> iterator = unexploredStates.iterator();
+            final DroidState state = iterator.next();
+            iterator.remove();
+            final long result = state.droid.move(state.direction);
+            if (result == 1) {
+                addToStateAllPossibleMoves(state.droid, state.step);
+            }
+            if (result == 2) {
+                visitedLocation.put(state.droid.getCurrentLocation(), 'O');
+            }
+        }
+        return visitedLocation;
+    }
+
+
 
 
 }
